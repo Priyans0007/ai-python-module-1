5d70c3d89feaeb11532b1c43bb9b6ea6227aec03
Priyanshu Priy <priyanshupriy9546@gmail.com>
2026-08-20 08:48:17 +0530

    Add functions for greeting and adding numbers

---
+++ functions.py
@@ -0,0 +1,12 @@
+# Python Functions
+
+def greet(name):
+    return "Hello, " + name + "!"
+
+def add_numbers(a, b):
+    return a + b
+
+print(greet("Priyanshu"))
+
+result = add_numbers(10, 20)
+print("Sum:", result)

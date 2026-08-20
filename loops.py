9626af0120bb1d3c9382dce064e2eca7ff1de9dd
Priyanshu Priy <priyanshupriy9546@gmail.com>
2026-08-20 08:47:35 +0530

    Add loops.py with for loop and multiplication table

---
+++ loops.py
@@ -0,0 +1,11 @@
+# Python Loops
+
+numbers = [1, 2, 3, 4, 5]
+
+print("Using for loop:")
+for number in numbers:
+    print(number)
+
+print("\nMultiplication table of 5:")
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
